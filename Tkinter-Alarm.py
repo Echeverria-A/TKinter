46ce9814ce57764119d1aa48019313ff270415de
Echeverria-A <anthony.echeverria@myds.us>
2017-01-23 10:45:24 -0800

    insert input box

--- Tkinter-Alarm.py
+++ Tkinter-Alarm.py
@@ -1,15 +1,27 @@
 from Tkinter import *
 import time
 from PIL import Image, ImageTk
- 
+import tkMessageBox
+
 root = Tk()
 root.title("Alarm Clock")
 root.configure(background='white')
 
 
+
+
+def alarm():
+   tkMessageBox.showinfo("Alarm", "Alarm Reached!")
+
+def getText():
+    global myTime
+    myString = inputBox.get()
+
+
+
 #clock
 time1 = ''
-clock = Label(root, font=('cosmic sans',20, 'bold',), bg='#f282db')
+clock = Label(root, font=('Times New Roman',30, 'bold',), bg='#f282db')
 clock.grid(row=1, column=0, sticky=EW, columnspan=6)
 def tick():
     global time1
@@ -18,6 +30,11 @@ def tick():
         time1 = time2
         clock.config(text=time2)
     clock.after(200, tick)
+
+
+
+
+    
 
 
 #menubar        
@@ -30,17 +47,14 @@ menubar.add_cascade(label="File", menu=filemenu)
 root.config(menu=menubar)
  
 #buttons
-button1 = Button(root, text="Hour",)
-button1.grid(row=3, column=0)
+#button1 = Button(root, text="Hour",)
+#button1.grid(row=3, column=0)
 
-button4 = Button(root, text="Minute",)
-button4.grid(row=3, column=3)
+#button4 = Button(root, text="Minute",)
+#button4.grid(row=3, column=3)
 
-button4 = Button(root, text="Set Alarm",)
-button4.grid(row=4, column=1 , columnspan=2)
-
-#text1 = Text(root)
-#text1.grid(row=5, column=1)
+#button4 = Button(root, text="Set Alarm",)
+#button4.grid(row=4, column=1 , columnspan=2)
 
 
 
@@ -48,22 +62,29 @@ button4.grid(row=4, column=1 , columnspan=2)
 
 
 #Up button image
-image = Image.open("up.png")
-image = image.resize((25,25,))
-photo = ImageTk.PhotoImage(image)
+#image = Image.open("up.png")
+#image = image.resize((25,25,))
+#photo = ImageTk.PhotoImage(image)
 
-button2= Button(root, text="B", image=photo)
-button2.image = photo
-button2.grid(row=3, column=1,)
+#button2= Button(root, text="B", image=photo)
+#button2.image = photo
+#button2.grid(row=3, column=1,)
 
 #down button image
-image = Image.open("down.png")
-image = image.resize((21,25,))
-photo = ImageTk.PhotoImage(image)
+#image = Image.open("down.png")
+#image = image.resize((21,25,))
+#photo = ImageTk.PhotoImage(image)
 
-button3= Button(root, text="B", image=photo)
-button3.image = photo
-button3.grid(row=3, column=2,)
+#button3= Button(root, text="B", image=photo)
+#button3.image = photo
+#button3.grid(row=3, column=2,)
+
+w=Entry(root, bg="white")
+w.grid(row=5,column=1)
+button5=Button(root, text="Set Alarm", command=getText)
+button5.grid(row=5,column=2)
+
+
 
 
 
