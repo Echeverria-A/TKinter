5e446a6ac6c6be72497c17e2602da88f1d0bc128
Echeverria-A <anthony.echeverria@myds.us>
2017-02-06 09:56:54 -0800

    yikes

--- Tkinter-Alarm.py
+++ Tkinter-Alarm.py
@@ -1,3 +1,4 @@
+
 from Tkinter import *
 import time
 from PIL import Image, ImageTk
@@ -47,14 +48,14 @@ menubar.add_cascade(label="File", menu=filemenu)
 root.config(menu=menubar)
  
 #buttons
-#button1 = Button(root, text="Hour",)
-#button1.grid(row=3, column=0)
+button1 = Button(root, text="Hour",)
+button1.grid(row=3, column=0)
 
-#button4 = Button(root, text="Minute",)
-#button4.grid(row=3, column=3)
+button4 = Button(root, text="Minute",)
+button4.grid(row=3, column=3)
 
-#button4 = Button(root, text="Set Alarm",)
-#button4.grid(row=4, column=1 , columnspan=2)
+button4 = Button(root, text="Set Alarm",)
+button4.grid(row=4, column=1 , columnspan=2)
 
 
 
@@ -62,28 +63,29 @@ root.config(menu=menubar)
 
 
 #Up button image
-#image = Image.open("up.png")
-#image = image.resize((25,25,))
-#photo = ImageTk.PhotoImage(image)
+image = Image.open("up.png")
+image = image.resize((25,25,))
+photo = ImageTk.PhotoImage(image)
 
-#button2= Button(root, text="B", image=photo)
-#button2.image = photo
-#button2.grid(row=3, column=1,)
+button2= Button(root, text="B", image=photo)
+button2.image = photo
+button2.grid(row=3, column=1,)
 
 #down button image
-#image = Image.open("down.png")
-#image = image.resize((21,25,))
-#photo = ImageTk.PhotoImage(image)
+image = Image.open("down.png")
+image = image.resize((21,25,))
+photo = ImageTk.PhotoImage(image)
 
-#button3= Button(root, text="B", image=photo)
-#button3.image = photo
-#button3.grid(row=3, column=2,)
+button3= Button(root, text="B", image=photo)
+button3.image = photo
+button3.grid(row=3, column=2,)
 
-w=Entry(root, bg="white")
-w.grid(row=5,column=1)
-button5=Button(root, text="Set Alarm", command=getText)
-button5.grid(row=5,column=2)
 
+#Entry Box Text
+#w=Entry(root, bg="white")
+#w.grid(row=5,column=1)
+#button5=Button(root, text="Set Alarm", command=getText)
+#button5.grid(row=5,column=2)
 
 
 
